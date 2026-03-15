6ce1fe1b447349931a1bbedf8a79f2fb2f02ff7b
user-user90 <wissalinas6@gmail.com>
2026-03-15 00:42:38 +0000

    Mise à jour du design et ajout des notifications toast

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import { client } from "@/sanity/lib/client";
-import HeroSection from "./_compenents/(Headers)/HeroSection";
+import HeroSection from "./_components/(headers)/HeroSection";
 
 const getData = async () => {
   // أزلنا [0] لكي نحصل على كل الأصناف ونستطيع عمل map
